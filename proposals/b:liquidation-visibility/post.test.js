f85a2cfe02dc5799fee6e43d3be783b7fd9de245
anilhelvaci <anilhelvaci96@gmail.com>
2024-02-21 15:01:36 +0300

    chore(liquidationVisibility): init work

---
+++ proposals/b:liquidation-visibility/post.test.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+
+/**
+ * 1. Add new collateral manager
+ * 2. Manipulate prices for the new collateral via oracles
+ * 3. Open a vault
+ * 4. Trigger liquidation
+ */
+
+test.serial('build-proposal', t => {
+  t.log('initial');
+  t.pass();
+});
