001a8ffd9053a580715f5ad7ce9cd4ebd0978894
alexanderem49 <alexanderem49@gmail.com>
2024-02-22 17:44:33 +0400

    chore(liquidationVisibility): read local addresses

--- proposals/b:liquidation-visibility/add-STARS.js
+++ proposals/b:liquidation-visibility/add-STARS.js
@@ -1,6 +1,7 @@
 import { makeHelpers } from '/usr/src/agoric-sdk/packages/deploy-script-support/src/helpers.js';
 import { defaultProposalBuilder as vaultProposalBuilder } from '/usr/src/agoric-sdk/packages/inter-protocol/scripts/add-collateral-core.js';
 import { defaultProposalBuilder as oraclesProposalBuilder } from '/usr/src/agoric-sdk/packages/inter-protocol/scripts/price-feed-core.js';
+import fs from 'fs';
 
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
 export const starsVaultProposalBuilder = async powers => {
@@ -17,6 +18,36 @@ export const starsVaultProposalBuilder = async powers => {
   });
 };
 
+/**
+ * @param {string} keyName gov1, gov2...
+ */
+export const getAgoricAddress = keyName => {
+  const pathInfo = `/root/.agoric/${keyName}.out`;
+  const keyData = fs.readFileSync(
+      pathInfo,
+      { encoding: 'utf-8', flag: 'r' },
+  );
+
+  const pathMnemonic = `/root/.agoric/${keyName}.key`;
+  const keyMnemonic = fs.readFileSync(
+      pathMnemonic,
+      { encoding: 'utf-8', flag: 'r' },
+  );
+
+  const nameExp = /name: (.*)\n/;
+  const typeExp = /type: (.*)\n/;
+  const addressExp = /address: (.*)\n/;
+  const pubkeyExp = /pubkey: (.*)\n/;
+
+  return {
+      name: nameExp.exec(keyData)[1],
+      type: typeExp.exec(keyData)[1],
+      address: addressExp.exec(keyData)[1],
+      pubkey: pubkeyExp.exec(keyData)[1],
+      mnemonic: keyMnemonic.split('\n')[0],
+  };
+}
+
 /** @type {import('@agoric/deploy-script-support/src/externalTypes.js').ProposalBuilder} */
 export const starsOraclesProposalBuilder = async powers => {
   return oraclesProposalBuilder(powers, {
@@ -26,9 +57,8 @@ export const starsOraclesProposalBuilder = async powers => {
     OUT_BRAND_LOOKUP: ['agoricNames', 'oracleBrand', 'USD'],
     OUT_BRAND_DECIMALS: 4,
     oracleAddresses: [
-      // copied from decentral-main-vaults-config.json
-      'agoric1krunjcqfrf7la48zrvdfeeqtls5r00ep68mzkr', // Make gov1
-      'agoric19uscwxdac6cf6z7d5e26e0jm0lgwstc47cpll8', // Make gov2
+      getAgoricAddress('gov1').address,
+      getAgoricAddress('gov2').address,
       'agoric144rrhh4m09mh7aaffhm6xy223ym76gve2x7y78',
       'agoric19d6gnr9fyp6hev4tlrg87zjrzsd5gzr5qlfq2p',
       'agoric1n4fcxsnkxe4gj6e24naec99hzmc4pjfdccy5nj',
