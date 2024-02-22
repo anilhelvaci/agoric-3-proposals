bedc80e415039a64106f7a906f0bc40e233e7514
alexanderem49 <alexanderem49@gmail.com>
2024-02-22 12:53:49 +0400

    chore(liquidationVisibility): update bundles

--- proposals/b:liquidation-visibility/test-liquidation-visibility.js
+++ proposals/b:liquidation-visibility/test-liquidation-visibility.js
@@ -26,12 +26,14 @@ import {
   makeFileRW,
 } from '@agoric/synthetic-chain/src/lib/webAsset.js';
 import {
+  bundleDetail,
   ensureISTForInstall, flags, getContractInfo,
   loadedBundleIds,
   testIncludes,
   txAbbr
 } from './core-eval-support.js';
 import { voteLatestProposalAndWait, waitForBlock } from '@agoric/synthetic-chain/src/lib/commonUpgradeHelpers.js';
+import { NonNullish } from '@agoric/synthetic-chain/src/lib/assert.js';
 
 /** @typedef {Awaited<ReturnType<typeof makeTestContext>>} TestContext */
 /** @type {import('ava').TestFn<TestContext>}} */
@@ -42,9 +44,12 @@ const assetInfo = {
   buildAssets: {
     coreEvalInfo: {
       evals: [
-        { permit: 'vaultFactory-permit.json', script: 'vaultFactory.js' },
+        { permit: 'upgrade-vaults-liq-visibility-permit.json', script: 'upgrade-vaults-liq-visibility.js' },
       ],
-      bundles: ['bundle-vaultFactory.json'],
+      bundles: [
+        'b1-7a5b067832fe1e968aca362ad713126737c3f0289dba7b527e0d23648b9419395c16399eed9358d5c555d29d2724561c12902dc2c960eca1bc4f0deee373a5c8.json',
+        'b1-6874b3846d9b293af2e687b168ac825dc19e027f949b10e9c766507db604246c90866428581b94e068779f4dabbf6dd9da5b4161e483a50c6525c8a53edc3025.json'
+      ],
     },
   },
 };
@@ -83,21 +88,6 @@ const staticConfig = {
     },
   },
   ...dappAPI,
-};
-
-const bundleDetail = async (src, bundleName) => {
-  const file = src.join(bundleName);
-  const [content, absPath] = await Promise.all([
-    file.readText(),
-    file.toString(),
-  ]);
-  const { endoZipBase64Sha512 } = JSON.parse(content);
-  return {
-    id: `b1-${endoZipBase64Sha512}`,
-    fileName: bundleName,
-    endoZipBase64Sha512,
-    absPath,
-  };
 };
 
 /**
@@ -158,10 +148,12 @@ test.serial('bundles not yet installed', async t => {
   for await (const { bundles, evals } of info) {
     t.log(evals[0].script, evals.length, 'eval', bundles.length, 'bundles');
 
-    const detail = await bundleDetail(src, bundles[0]);
-    console.log({ detail });
-    const { id } = detail;
-    testIncludes(t, id, loaded, 'loaded bundles', false);
+    for await (const bundle of bundles) {
+      const detail = bundleDetail(bundle);
+      console.log({ detail });
+      const { id } = detail;
+      testIncludes(t, id, loaded, 'loaded bundles', false);
+    }
   }
 });
 
@@ -209,9 +201,8 @@ test.serial('ensure bundles installed', async t => {
   for await (const { bundles } of staticConfig.buildInfo) {
     todo += bundles.length;
     for await (const bundle of bundles) {
-      const { id, endoZipBase64Sha512, absPath } = await bundleDetail(
-        src,
-        bundle,
+      const { id, endoZipBase64Sha512, fileName } = bundleDetail(
+        bundle
       );
       if (loaded.includes(id)) {
         t.log('bundle already installed', id);
@@ -220,7 +211,7 @@ test.serial('ensure bundles installed', async t => {
       }
 
       const result = await agd.tx(
-        ['swingset', 'install-bundle', `@${absPath}`, '--gas', '120000000'],
+        ['swingset', 'install-bundle', `@./assets/${fileName}`, '--gas', '120000000'],
         { from, chainId, yes: true },
       );
       t.log(txAbbr(result));
@@ -266,7 +257,7 @@ test.serial('core eval proposal passes', async t => {
   const { buildInfo } = staticConfig;
   for (const { bundles } of buildInfo) {
     for (const bundle of bundles) {
-      const { id } = await bundleDetail(src, bundle);
+      const { id } = bundleDetail(bundle);
       testIncludes(t, id, loaded, 'loaded bundles');
     }
   }
