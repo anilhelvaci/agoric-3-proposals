2b55c90040bf3c66200cb119b58732907034a786
anilhelvaci <anilhelvaci96@gmail.com>
2024-03-08 11:36:38 +0300

    fix(liquidationVisibility): fix bundle names in test-liquidation-visibility.js and add interestedProperty in vstorage-helpers.js
    Refs: #10

--- proposals/b:liquidation-visibility/helpers/vstorage-helpers.js
+++ proposals/b:liquidation-visibility/helpers/vstorage-helpers.js
@@ -6,12 +6,17 @@ import { getContractInfo, agoric, agd } from "@agoric/synthetic-chain";
  * Especially for the kind = 'data' where unmarshalling is required.
  */
 
-const [,,kind, path] = process.argv;
+const [,,kind, path, interestedProperty] = process.argv;
 
 if (kind === 'data') {
   const result = await getContractInfo(path, { agoric, prefix: '' });
   console.log('Data: ');
   console.log(result);
+
+  if (interestedProperty) {
+    console.log('Here is the property you are interested: ');
+    console.log(result.current[interestedProperty]);
+  }
 } else if (kind === 'children') {
   const children = await agd.query('vstorage', kind, path);
   console.log('Children: ');
